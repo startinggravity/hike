112f60212508264f278b9f535b28bf7d28e80421
Jim Smith <jim@startinggravity.com>
2020-09-02 19:56:21 -0400

    Reconfigure home background.

--- gatsby/src/components/HeroVideo/index.js
+++ gatsby/src/components/HeroVideo/index.js
@@ -1,15 +1,13 @@
 import React from "react"
-import bgImage from "../../assets/images/no_video_background.jpg"
+// import bgImage from "../../assets/images/no_video_background.jpg"
+import bgImage from "../../assets/images/blog_loop_first_frame.jpg"
 import firstFrame from "../../assets/images/blog_loop_first_frame.jpg"
 import video from "../../assets/video/blog_loop.mp4"
+import StyledArtDirectedBackground from "../ArtDirectedBackground"
 
 const HeroVideo = (title, subtitle) => (
   <div
     className="hero-video"
-    style={{
-      // backgroundColor: `tomato`,
-      backgroundImage: "url(" + bgImage + ")",
-    }}
   >
     <video
       className="hero-video__player"
@@ -17,15 +15,18 @@ const HeroVideo = (title, subtitle) => (
       muted
       loop=""
       poster={firstFrame}
+      preload="auto"
     >
       <source className="video" src={video} type="video/mp4" />
     </video>
+
     <div className="hero-video__text">
       <h2 className="hero-video__quote">
         "In every walk with nature, one receives far more than he seeks."
       </h2>
       <h3 className="hero-video__quote-attribution">John Muir</h3>
     </div>
+    <StyledArtDirectedBackground />
   </div>
 )
 
