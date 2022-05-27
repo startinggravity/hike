9645ca9217048995cd88a40ddefc0e13389be05f
Jim Smith <jim@startinggravity.com>
2022-05-26 21:03:28 -0400

    Fix meta tags in blog posts.

--- gatsby-rebuild/src/templates/NodeBlogTemplate/index.js
+++ gatsby-rebuild/src/templates/NodeBlogTemplate/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { getSrc } from "gatsby-plugin-image"
 import uuid from "react-uuid"
 import { componentResolver } from "../../helpers/component-resolver"
 import Cover from "../../components/cover"
@@ -18,7 +17,7 @@ const NodeBlogTemplate = ({ data: { node }, pageContext }) => {
   let fullTitle = ""
   // let gearTitle = title + ": " + subtitle
   const blogType = node.type.tid
-  const socialImageSrc = getSrc(node.relationships.social.gatsbyImage)
+  const socialImageSrc = node.relationships.social.publicUrl
   let contentType = ""
 
   if (node.type.tid === 1) {
@@ -89,11 +88,7 @@ const NodeBlogTemplate = ({ data: { node }, pageContext }) => {
     <>
       <Seo
         title={fullTitle}
-        description={
-          `A collection of blog posts from Gravity's ` +
-          subTitle +
-          ` thru-hike.`
-        }
+        description={subTitle}
         nodePath={node.path.alias}
         nodeImage={socialImageSrc}
       />
@@ -171,6 +166,7 @@ export const query = graphql`
         }
         social: field_main_image {
           gatsbyImage(layout: FIXED, width: 600, height: 338, formats: JPG)
+          publicUrl
         }
         field_body_elements {
           __typename
