2d6eb0f6df00eaff1e667490fc101e2316a90b32
Jim Smith <jim@startinggravity.com>
2022-12-14 17:31:42 -0500

    Override so pages will always scroll to top.

--- gatsby-rebuild/gatsby-browser.js
+++ gatsby-rebuild/gatsby-browser.js
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import './src/styles/globals.css'
+import('./src/styles/globals.css')
+
+export function shouldUpdateScroll(prevRouterProps, { location }) {
+  window.scrollTo(0, 0)
+  const body = document.getElementsByTagName("body")[0]
+  body.scrollTop = 0
+  return false
+}
