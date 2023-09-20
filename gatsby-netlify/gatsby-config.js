70d9162e435b8276758f8b0afe789dc636047118
Jim Smith <jim@startinggravity.com>
2023-09-20 15:43:22 -0400

    Specify image formats in config.

--- gatsby-netlify/gatsby-config.js
+++ gatsby-netlify/gatsby-config.js
@@ -52,7 +52,23 @@ module.exports = {
       },
     },
     `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `blurred`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
